deb056968bf7be5c72a993bcf5a70ef810976340
ehmkey <ehmkey@gmail.com>
2021-04-06 20:57:02 +0200

    add init overview

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { AppointmentService } from '../shared/appointment.service';
+import { AngularFireList } from '@angular/fire/database';
+import { Appointment } from '../shared/Appointment';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  appointments: AngularFireList<Appointment>;
+  records;
+  constructor(private aptService: AppointmentService) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    //  this.addrecord = {type :'', description :'', amount: null}
+    this.records = this.aptService
+      .getAll()
+      .valueChanges()
+  }
 }
